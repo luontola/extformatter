0bbc930b2650dafd7359d4ecca4dd44fafc7e98a
Esko Luontola <esko.luontola@gmail.com>
2007-11-29 00:47:35 +0000

    Improved readability

--- install-idea-files.bat
+++ install-idea-files.bat
@@ -1,8 +1,12 @@
+@echo off
 rem complete your IDEA Home folder here
 set IDEA_HOME=C:\Program Files\JetBrains\IntelliJ IDEA 7.0.1
 set IDEA_VERSION=7.0.1.7364
-echo %IDEA_HOME%
+echo.
+echo Installing packages of IDEA version %IDEA_VERSION% from "%IDEA_HOME%" to the local Maven repository...
+echo.
 call mvn install:install-file -DgroupId=com.intellij.idea -Dpackaging=jar -Dfile="%IDEA_HOME%\lib\openapi.jar" -DartifactId=openapi -Dversion=%IDEA_VERSION%
 call mvn install:install-file -DgroupId=com.intellij.idea -Dpackaging=jar -Dfile="%IDEA_HOME%\lib\annotations.jar" -DartifactId=annotations -Dversion=%IDEA_VERSION%
 call mvn install:install-file -DgroupId=com.intellij.idea -Dpackaging=jar -Dfile="%IDEA_HOME%\lib\extensions.jar" -DartifactId=extensions -Dversion=%IDEA_VERSION%
+echo.
 pause

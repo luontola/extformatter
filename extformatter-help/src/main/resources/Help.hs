cd6119badfc0f37cedfe2e366be23d1c4967f905
Esko Luontola <esko.luontola@gmail.com>
2007-12-07 18:22:55 +0000

    Added help files from MavenProjectHelper as-is

---
+++ extformatter-help/src/main/resources/Help.hs
@@ -0,0 +1,25 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+        PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+        "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="1.0">
+    <!-- title -->
+    <title>Maven Project Helper Help</title>
+
+    <!-- maps -->
+    <maps>
+        <homeID>mavanprojecthelper</homeID>
+        <mapref location="Map.xml"/>
+    </maps>
+
+    <!-- views -->
+
+    <view>
+        <name>TOC</name>
+        <label>Table Of Contents</label>
+        <type>javax.help.TOCView</type>
+        <data>HelpTOC.xml</data>
+    </view>
+
+</helpset>

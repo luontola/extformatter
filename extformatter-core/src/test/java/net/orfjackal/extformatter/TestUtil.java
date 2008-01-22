17e09a07bee94f8b0ae96aa85e92903a0f13a1c0
Esko Luontola <esko.luontola@gmail.com>
2008-01-22 21:55:11 +0000

    Updated copyright year to 2007-2008

--- extformatter-core/src/test/java/net/orfjackal/extformatter/TestUtil.java
+++ extformatter-core/src/test/java/net/orfjackal/extformatter/TestUtil.java
@@ -1,6 +1,6 @@
 /*
  * External Code Formatter
- * Copyright (c) 2008 Esko Luontola, www.orfjackal.net
+ * Copyright (c) 2007-2008 Esko Luontola, www.orfjackal.net
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

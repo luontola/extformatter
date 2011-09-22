e338e4d987d3303f073027b258f6a348e858ae89
Anders Wallgren <anders_wallgren@alum.mit.edu>
2011-09-22 14:29:41 -0700

    Rebuild with 108.1333

--- extformatter-idea-plugin/src/main/java/net/orfjackal/extformatter/plugin/DelegatingCodeStyleManager.java
+++ extformatter-idea-plugin/src/main/java/net/orfjackal/extformatter/plugin/DelegatingCodeStyleManager.java
@@ -24,101 +24,178 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.codeStyle.Indent;
 import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.ThrowableRunnable;
 
 /**
- * Wrapper for intercepting the method calls to a {@link CodeStyleManager} instance.
+ * Wrapper for intercepting the method calls to a {@link CodeStyleManager}
+ * instance.
  *
  * @author Esko Luontola
  * @since 2.12.2007
  */
 @SuppressWarnings({"deprecation"})
-public class DelegatingCodeStyleManager extends CodeStyleManager {
+public class DelegatingCodeStyleManager
+    extends CodeStyleManager
+{
+    @NotNull
+    private final CodeStyleManager target;
 
-    @NotNull private final CodeStyleManager target;
-
-    public DelegatingCodeStyleManager(@NotNull CodeStyleManager target) {
+    public DelegatingCodeStyleManager(@NotNull CodeStyleManager target)
+    {
         this.target = target;
     }
 
     @NotNull
-    public CodeStyleManager getTarget() {
+    public CodeStyleManager getTarget()
+    {
         return target;
     }
 
-    // Generated delegate methods
+    @Override
+    public void performActionWithFormatterDisabled(Runnable runnable)
+    {
+        target.performActionWithFormatterDisabled(runnable);
+    }
+
+    @Override
+    public <T extends Throwable> void performActionWithFormatterDisabled(ThrowableRunnable<T> tThrowableRunnable)
+        throws T
+    {
+        target.performActionWithFormatterDisabled(tThrowableRunnable);
+    }
+
+    @Override
+    public <T> T performActionWithFormatterDisabled(Computable<T> tComputable)
+    {
+        return target.performActionWithFormatterDisabled(tComputable);
+    }
 
     @NotNull
-    public Project getProject() {
+    public Project getProject()
+    {
         return target.getProject();
     }
 
     @NotNull
-    public PsiElement reformat(@NotNull PsiElement element) throws IncorrectOperationException {
+    public PsiElement reformat(@NotNull PsiElement element)
+        throws IncorrectOperationException
+    {
         return target.reformat(element);
     }
 
     @NotNull
-    public PsiElement reformat(@NotNull PsiElement element, boolean canChangeWhiteSpacesOnly) throws IncorrectOperationException {
+    public PsiElement reformat(
+        @NotNull PsiElement element,
+        boolean canChangeWhiteSpacesOnly)
+        throws IncorrectOperationException
+    {
         return target.reformat(element, canChangeWhiteSpacesOnly);
     }
 
-    public PsiElement reformatRange(@NotNull PsiElement element, int startOffset, int endOffset) throws IncorrectOperationException {
+    public PsiElement reformatRange(
+        @NotNull PsiElement element,
+        int startOffset,
+        int endOffset)
+        throws IncorrectOperationException
+    {
         return target.reformatRange(element, startOffset, endOffset);
     }
 
-    public PsiElement reformatRange(@NotNull PsiElement element, int startOffset, int endOffset, boolean canChangeWhiteSpacesOnly) throws IncorrectOperationException {
-        return target.reformatRange(element, startOffset, endOffset, canChangeWhiteSpacesOnly);
+    public PsiElement reformatRange(
+        @NotNull PsiElement element,
+        int startOffset,
+        int endOffset,
+        boolean canChangeWhiteSpacesOnly)
+        throws IncorrectOperationException
+    {
+        return target.reformatRange(element, startOffset, endOffset,
+            canChangeWhiteSpacesOnly);
     }
 
-    public void reformatText(@NotNull PsiFile element, int startOffset, int endOffset) throws IncorrectOperationException {
+    public void reformatText(
+        @NotNull PsiFile element,
+        int startOffset,
+        int endOffset)
+        throws IncorrectOperationException
+    {
         target.reformatText(element, startOffset, endOffset);
     }
 
-    public void adjustLineIndent(@NotNull PsiFile file, TextRange rangeToAdjust) throws IncorrectOperationException {
+    public void adjustLineIndent(
+        @NotNull PsiFile file,
+        TextRange rangeToAdjust)
+        throws IncorrectOperationException
+    {
         target.adjustLineIndent(file, rangeToAdjust);
     }
 
-    public int adjustLineIndent(@NotNull PsiFile file, int offset) throws IncorrectOperationException {
+    public int adjustLineIndent(
+        @NotNull PsiFile file,
+        int offset)
+        throws IncorrectOperationException
+    {
         return target.adjustLineIndent(file, offset);
     }
 
-    public int adjustLineIndent(@NotNull Document document, int offset) {
+    public int adjustLineIndent(
+        @NotNull Document document,
+        int offset)
+    {
         return target.adjustLineIndent(document, offset);
     }
 
-    public boolean isLineToBeIndented(@NotNull PsiFile file, int offset) {
+    public boolean isLineToBeIndented(
+        @NotNull PsiFile file,
+        int offset)
+    {
         return target.isLineToBeIndented(file, offset);
     }
 
     @Nullable
-    public String getLineIndent(@NotNull PsiFile file, int offset) {
+    public String getLineIndent(
+        @NotNull PsiFile file,
+        int offset)
+    {
         return target.getLineIndent(file, offset);
     }
 
     @Nullable
-    public String getLineIndent(@NotNull Editor editor) {
+    public String getLineIndent(@NotNull Editor editor)
+    {
         return target.getLineIndent(editor);
     }
 
-    public Indent getIndent(String text, FileType fileType) {
+    public Indent getIndent(
+        String text,
+        FileType fileType)
+    {
         return target.getIndent(text, fileType);
     }
 
-    public String fillIndent(Indent indent, FileType fileType) {
+    public String fillIndent(
+        Indent indent,
+        FileType fileType)
+    {
         return target.fillIndent(indent, fileType);
     }
 
-    public Indent zeroIndent() {
+    public Indent zeroIndent()
+    {
         return target.zeroIndent();
     }
 
-    public void reformatNewlyAddedElement(@NotNull ASTNode block, @NotNull ASTNode addedElement) throws IncorrectOperationException {
+    public void reformatNewlyAddedElement(
+        @NotNull ASTNode block,
+        @NotNull ASTNode addedElement)
+        throws IncorrectOperationException
+    {
         target.reformatNewlyAddedElement(block, addedElement);
     }
 
--- extformatter-idea-plugin/src/test/java/net/orfjackal/extformatter/plugin/ExternalizedCodeStyleManagerSpec.java
+++ extformatter-idea-plugin/src/test/java/net/orfjackal/extformatter/plugin/ExternalizedCodeStyleManagerSpec.java
@@ -1,102 +1,157 @@
-/*
- * External Code Formatter
- * Copyright (c) 2007-2009  Esko Luontola, www.orfjackal.net
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
+// ExternalizedCodeStyleManagerSpec.java --
+//
+// ExternalizedCodeStyleManagerSpec.java is part of ElectricCommander.
+//
+// Copyright (c) 2005-2011 Electric Cloud, Inc.
+// All rights reserved.
+//
 
 package net.orfjackal.extformatter.plugin;
 
-import com.intellij.psi.*;
-import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.util.IncorrectOperationException;
+import java.io.File;
+
+import org.jmock.Expectations;
+import org.junit.runner.RunWith;
 import jdave.Specification;
 import jdave.junit4.JDaveRunner;
 import net.orfjackal.extformatter.CodeFormatter;
-import org.jmock.Expectations;
-import org.junit.runner.RunWith;
-
-import java.io.File;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.util.IncorrectOperationException;
 
 /**
- * @author Esko Luontola
- * @since 6.12.2007
+ * @author  Esko Luontola
+ * @since   6.12.2007
  */
 @RunWith(JDaveRunner.class)
-public class ExternalizedCodeStyleManagerSpec extends Specification<ExternalizedCodeStyleManager> {
+public class ExternalizedCodeStyleManagerSpec
+    extends Specification<ExternalizedCodeStyleManager>
+{
+
+    //~ Static fields/initializers ---------------------------------------------
 
     /**
-     * For now, these tests exist only for documentation purposes,
-     * because I haven't figured out a way to run these tests.
-     * IDEA has some unit testing support (com.intellij.testFramework.IdeaTestCase)
-     * but I haven't found enough examples on how to use it.
+     * For now, these tests exist only for documentation purposes, because I
+     * haven't figured out a way to run these tests. IDEA has some unit testing
+     * support (com.intellij.testFramework.IdeaTestCase) but I haven't found
+     * enough examples on how to use it.
      */
     private static boolean TEST_DISABLED = true;
 
-    public class WhenTheFileIsInLocalFileSystemAndItsTypeIsSupported {
+    //~ Inner Classes ----------------------------------------------------------
 
-        private PsiFile file;
-        private CodeStyleManager original;
-        private CodeFormatter replacement;
+    public class WhenTheFileIsInLocalFileSystemAndItsTypeIsSupported
+    {
+
+        //~ Instance fields ----------------------------------------------------
+
+        private PsiFile                      file;
+        private CodeStyleManager             original;
+        private CodeFormatter                replacement;
         private ExternalizedCodeStyleManager manager;
 
-        public ExternalizedCodeStyleManager create() throws Exception {
-            if (TEST_DISABLED) return null;
-            file = mock(PsiJavaFile.class); // 'file' is a real writable file in local file system
-            original = mock(CodeStyleManager.class);
+        //~ Methods ------------------------------------------------------------
+
+        public ExternalizedCodeStyleManager create()
+            throws Exception
+        {
+
+            if (TEST_DISABLED) {
+                return null;
+            }
+
+            file        = mock(PsiJavaFile.class);    // 'file' is a real
+                                                      // writable file in local
+                                                      // file system
+            original    = mock(CodeStyleManager.class);
             replacement = mock(CodeFormatter.class);
-            manager = new ExternalizedCodeStyleManager(original, replacement);
+            manager     = new ExternalizedCodeStyleManager(original,
+                    replacement);
+
             return manager;
         }
 
-        public void reformattingWholeFileShouldUseTheReplacementFormatter() throws IncorrectOperationException {
-            if (TEST_DISABLED) return;
-            checking(new Expectations() {{
-                one (replacement).reformatOne(new File("Foo.java"));
-            }});
-            manager.reformatText(file, 0, 100);
+        public void reformattingSelectedTextShouldFallBackToTheOriginalCodeStyleManager()
+            throws IncorrectOperationException
+        {
+
+            if (TEST_DISABLED) {
+                return;
+            }
+
+            checking(new Expectations() {
+
+                    {
+                        one(original).reformatText(file, 30, 40);
+                    }
+                });
+            manager.reformatText(file, 30, 40);
         }
 
-        public void reformattingSelectedTextShouldFallBackToTheOriginalCodeStyleManager() throws IncorrectOperationException {
-            if (TEST_DISABLED) return;
-            checking(new Expectations() {{
-                one (original).reformatText(file, 30, 40);
-            }});
-            manager.reformatText(file, 30, 40);
+        public void reformattingWholeFileShouldUseTheReplacementFormatter()
+            throws IncorrectOperationException
+        {
+
+            if (TEST_DISABLED) {
+                return;
+            }
+
+            checking(new Expectations() {
+
+                    {
+                        one(replacement).reformatOne(new File("Foo.java"));
+                    }
+                });
+            manager.reformatText(file, 0, 100);
         }
     }
 
-    public class WhenTheFileIsNotSupported {
+    public class WhenTheFileIsNotSupported
+    {
 
-        private PsiFile file;
-        private CodeStyleManager original;
+        //~ Instance fields ----------------------------------------------------
+
+        private PsiFile                      file;
+        private CodeStyleManager             original;
         private ExternalizedCodeStyleManager manager;
 
-        public ExternalizedCodeStyleManager create() throws Exception {
-            if (TEST_DISABLED) return null;
-            file = mock(XmlFile.class);
+        //~ Methods ------------------------------------------------------------
+
+        public ExternalizedCodeStyleManager create()
+            throws Exception
+        {
+
+            if (TEST_DISABLED) {
+                return null;
+            }
+
+            file     = mock(XmlFile.class);
             original = mock(CodeStyleManager.class);
+
             CodeFormatter replacement = mock(CodeFormatter.class);
+
             manager = new ExternalizedCodeStyleManager(original, replacement);
+
             return manager;
         }
 
-        public void shouldFallBackToTheOriginalCodeStyleManager() throws IncorrectOperationException {
-            if (TEST_DISABLED) return;
-            checking(new Expectations() {{
-                one (original).reformatText(file, 0, 100);
-            }});
+        public void shouldFallBackToTheOriginalCodeStyleManager()
+            throws IncorrectOperationException
+        {
+
+            if (TEST_DISABLED) {
+                return;
+            }
+
+            checking(new Expectations() {
+
+                    {
+                        one(original).reformatText(file, 0, 100);
+                    }
+                });
             manager.reformatText(file, 0, 100);
         }
     }

c609956417a0fc16edfd01401e14f32cbff0c0dd
Anders Wallgren <anders_wallgren@alum.mit.edu>
2011-12-05 14:52:32 -0800

    Rebuild with 11.0 RC2

--- extformatter-idea-plugin/src/main/java/net/orfjackal/extformatter/plugin/DelegatingCodeStyleManager.java
+++ extformatter-idea-plugin/src/main/java/net/orfjackal/extformatter/plugin/DelegatingCodeStyleManager.java
@@ -1,35 +1,30 @@
-/*
- * External Code Formatter
- * Copyright (c) 2007-2009  Esko Luontola, www.orfjackal.net
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
+// DelegatingCodeStyleManager.java --
+//
+// DelegatingCodeStyleManager.java is part of ElectricCommander.
+//
+// Copyright (c) 2005-2011 Electric Cloud, Inc.
+// All rights reserved.
+//
 
 package net.orfjackal.extformatter.plugin;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
 import com.intellij.lang.ASTNode;
+
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.TextRange;
+
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.codeStyle.Indent;
+
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.ThrowableRunnable;
 
@@ -37,171 +32,171 @@ import com.intellij.util.ThrowableRunnable;
  * Wrapper for intercepting the method calls to a {@link CodeStyleManager}
  * instance.
  *
- * @author Esko Luontola
- * @since 2.12.2007
+ * @author  Esko Luontola
+ * @since   2.12.2007
  */
 @SuppressWarnings({"deprecation"})
 public class DelegatingCodeStyleManager
     extends CodeStyleManager
 {
-    @NotNull
-    private final CodeStyleManager target;
+
+    //~ Instance fields --------------------------------------------------------
+
+    @NotNull private final CodeStyleManager m_target;
+
+    //~ Constructors -----------------------------------------------------------
 
     public DelegatingCodeStyleManager(@NotNull CodeStyleManager target)
     {
-        this.target = target;
+        m_target = target;
     }
 
-    @NotNull
-    public CodeStyleManager getTarget()
+    //~ Methods ----------------------------------------------------------------
+
+    @Override public void adjustLineIndent(
+            @NotNull PsiFile file,
+            TextRange        rangeToAdjust)
+        throws IncorrectOperationException
     {
-        return target;
+        m_target.adjustLineIndent(file, rangeToAdjust);
     }
 
-    @Override
-    public void performActionWithFormatterDisabled(Runnable runnable)
+    @Override public int adjustLineIndent(
+            @NotNull PsiFile file,
+            int              offset)
+        throws IncorrectOperationException
     {
-        target.performActionWithFormatterDisabled(runnable);
+        return m_target.adjustLineIndent(file, offset);
     }
 
-    @Override
-    public <T extends Throwable> void performActionWithFormatterDisabled(ThrowableRunnable<T> tThrowableRunnable)
+    @Override public int adjustLineIndent(
+            @NotNull Document document,
+            int               offset)
+    {
+        return m_target.adjustLineIndent(document, offset);
+    }
+
+    @Override public String fillIndent(
+            Indent   indent,
+            FileType fileType)
+    {
+        return m_target.fillIndent(indent, fileType);
+    }
+
+    @Override public void performActionWithFormatterDisabled(Runnable runnable)
+    {
+        m_target.performActionWithFormatterDisabled(runnable);
+    }
+
+    @Override public <T extends Throwable> void performActionWithFormatterDisabled(
+            ThrowableRunnable<T> tThrowableRunnable)
         throws T
     {
-        target.performActionWithFormatterDisabled(tThrowableRunnable);
+        m_target.performActionWithFormatterDisabled(tThrowableRunnable);
     }
 
-    @Override
-    public <T> T performActionWithFormatterDisabled(Computable<T> tComputable)
+    @Override public <T> T performActionWithFormatterDisabled(
+            Computable<T> tComputable)
     {
-        return target.performActionWithFormatterDisabled(tComputable);
+        return m_target.performActionWithFormatterDisabled(tComputable);
     }
 
-    @NotNull
-    public Project getProject()
-    {
-        return target.getProject();
-    }
-
-    @NotNull
-    public PsiElement reformat(@NotNull PsiElement element)
+    @NotNull @Override public PsiElement reformat(@NotNull PsiElement element)
         throws IncorrectOperationException
     {
-        return target.reformat(element);
+        return m_target.reformat(element);
     }
 
-    @NotNull
-    public PsiElement reformat(
-        @NotNull PsiElement element,
-        boolean canChangeWhiteSpacesOnly)
+    @NotNull @Override public PsiElement reformat(
+            @NotNull PsiElement element,
+            boolean             canChangeWhiteSpacesOnly)
         throws IncorrectOperationException
     {
-        return target.reformat(element, canChangeWhiteSpacesOnly);
+        return m_target.reformat(element, canChangeWhiteSpacesOnly);
     }
 
-    public PsiElement reformatRange(
-        @NotNull PsiElement element,
-        int startOffset,
-        int endOffset)
+    @Override public void reformatNewlyAddedElement(
+            @NotNull ASTNode block,
+            @NotNull ASTNode addedElement)
         throws IncorrectOperationException
     {
-        return target.reformatRange(element, startOffset, endOffset);
+        m_target.reformatNewlyAddedElement(block, addedElement);
     }
 
-    public PsiElement reformatRange(
-        @NotNull PsiElement element,
-        int startOffset,
-        int endOffset,
-        boolean canChangeWhiteSpacesOnly)
+    @Override public PsiElement reformatRange(
+            @NotNull PsiElement element,
+            int                 startOffset,
+            int                 endOffset)
         throws IncorrectOperationException
     {
-        return target.reformatRange(element, startOffset, endOffset,
+        return m_target.reformatRange(element, startOffset, endOffset);
+    }
+
+    @Override public PsiElement reformatRange(
+            @NotNull PsiElement element,
+            int                 startOffset,
+            int                 endOffset,
+            boolean             canChangeWhiteSpacesOnly)
+        throws IncorrectOperationException
+    {
+        return m_target.reformatRange(element, startOffset, endOffset,
             canChangeWhiteSpacesOnly);
     }
 
-    public void reformatText(
-        @NotNull PsiFile element,
-        int startOffset,
-        int endOffset)
+    @Override public void reformatText(
+            @NotNull PsiFile element,
+            int              startOffset,
+            int              endOffset)
         throws IncorrectOperationException
     {
-        target.reformatText(element, startOffset, endOffset);
+        m_target.reformatText(element, startOffset, endOffset);
     }
 
-    public void adjustLineIndent(
-        @NotNull PsiFile file,
-        TextRange rangeToAdjust)
-        throws IncorrectOperationException
+    @Override public Indent zeroIndent()
     {
-        target.adjustLineIndent(file, rangeToAdjust);
+        return m_target.zeroIndent();
     }
 
-    public int adjustLineIndent(
-        @NotNull PsiFile file,
-        int offset)
-        throws IncorrectOperationException
+    @Override public Indent getIndent(
+            String   text,
+            FileType fileType)
     {
-        return target.adjustLineIndent(file, offset);
+        return m_target.getIndent(text, fileType);
     }
 
-    public int adjustLineIndent(
-        @NotNull Document document,
-        int offset)
+    @Nullable @Override public String getLineIndent(
+            @NotNull PsiFile file,
+            int              offset)
     {
-        return target.adjustLineIndent(document, offset);
+        return m_target.getLineIndent(file, offset);
     }
 
-    public boolean isLineToBeIndented(
-        @NotNull PsiFile file,
-        int offset)
+    @Override public String getLineIndent(
+            @NotNull Document document,
+            int               offset)
     {
-        return target.isLineToBeIndented(file, offset);
+        return m_target.getLineIndent(document, offset);
     }
 
-    @Nullable
-    public String getLineIndent(
-        @NotNull PsiFile file,
-        int offset)
+    @NotNull @Override public Project getProject()
     {
-        return target.getLineIndent(file, offset);
+        return m_target.getProject();
     }
 
-    @Nullable
-    public String getLineIndent(@NotNull Editor editor)
+    @NotNull public CodeStyleManager getTarget()
     {
-        return target.getLineIndent(editor);
+        return m_target;
     }
 
-    public Indent getIndent(
-        String text,
-        FileType fileType)
+    @Override public boolean isLineToBeIndented(
+            @NotNull PsiFile file,
+            int              offset)
     {
-        return target.getIndent(text, fileType);
+        return m_target.isLineToBeIndented(file, offset);
     }
 
-    public String fillIndent(
-        Indent indent,
-        FileType fileType)
+    @Override public boolean isSequentialProcessingAllowed()
     {
-        return target.fillIndent(indent, fileType);
-    }
-
-    public Indent zeroIndent()
-    {
-        return target.zeroIndent();
-    }
-
-    public void reformatNewlyAddedElement(
-        @NotNull ASTNode block,
-        @NotNull ASTNode addedElement)
-        throws IncorrectOperationException
-    {
-        target.reformatNewlyAddedElement(block, addedElement);
-    }
-
-    @Override
-    public boolean isSequentialProcessingAllowed()
-    {
-        return target.isSequentialProcessingAllowed();
+        return m_target.isSequentialProcessingAllowed();
     }
 }
